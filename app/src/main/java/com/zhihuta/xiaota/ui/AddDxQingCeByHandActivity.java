37c26637a5d03176aeccbfdec95ee79b57ed618f
wu-xuemin <wuxuemin2000@126.com>
2020-10-14 15:28:12 +0800

    手动添加电缆截面下拉选择芯数等

--- app/src/main/java/com/zhihuta/xiaota/ui/AddDxQingCeByHandActivity.java
+++ app/src/main/java/com/zhihuta/xiaota/ui/AddDxQingCeByHandActivity.java
@@ -5,10 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.zhihuta.xiaota.R;
 
 public class AddDxQingCeByHandActivity extends AppCompatActivity {
+
+    private static final String[] mXinshuXuanXiang = {"单芯","2芯","3芯","4芯","5芯","6芯","其他"};
+    private Spinner spinnerXinshu;
+    private ArrayAdapter<String> adapterXinShu;
+
+    private static final String[] mJieMianXuanxXiang = {"直径1mm","直径2mm","直径3mm","直径4mm","直径5mm","直径6mm","其他"};
+    private Spinner spinnerJieMian;
+    private ArrayAdapter<String> adapterJieMian;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +33,65 @@ public class AddDxQingCeByHandActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        /// 芯数 下拉
+        spinnerXinshu = (Spinner) findViewById(R.id.spinner_xinshu);
+        //将可选内容与ArrayAdapter连接起来
+        adapterXinShu = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, mXinshuXuanXiang);
+
+        //设置下拉列表的风格
+        adapterXinShu.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //将adapter 添加到spinner中
+        spinnerXinshu.setAdapter(adapterXinShu);
+
+        //添加事件Spinner事件监听
+        spinnerXinshu.setOnItemSelectedListener(new SpinnerSelectedListenerXinShu());
+
+        //设置默认值
+        spinnerXinshu.setVisibility(View.VISIBLE);
+
+        /// 截面 下拉
+        spinnerJieMian = (Spinner) findViewById(R.id.spinner_jiemian);
+        //将可选内容与ArrayAdapter连接起来
+        adapterJieMian = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, mJieMianXuanxXiang);
+
+        //设置下拉列表的风格
+        adapterJieMian.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //将adapter 添加到spinner中
+        spinnerJieMian.setAdapter(adapterJieMian);
+
+        //添加事件Spinner事件监听
+        spinnerJieMian.setOnItemSelectedListener(new SpinnerSelectedListenerJieMian());
+
+        //设置默认值
+        spinnerJieMian.setVisibility(View.VISIBLE);
+
     }
+    //使用数组形式操作
+    class SpinnerSelectedListenerXinShu implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                                   long arg3) {
+//            view.setText("你的选择是："+m[arg2]);
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
+    }
+
+
+    class SpinnerSelectedListenerJieMian implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                                   long arg3) {
+//            view.setText("你的选择是："+m[arg2]);
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
